68466a6fab2ad361eb5ddc9819154e6b2b3da7b4
Maayer Hassan <hassanmaayer@gmail.com>
2025-05-06 14:08:35 +0500

    Delete Project directory

--- Project/utils/rag_pipeline.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-qa_pipeline = pipeline("question-answering", model="deepset/roberta-base-squad2")
-
-def generate_answer(question, context_chunks):
-    context = " ".join(context_chunks)
-    result = qa_pipeline(question=question, context=context)
-    return result["answer"]
